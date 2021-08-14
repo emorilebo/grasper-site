ac08a7fe21d3014e4dbc94da1c1ec8ceb9d4f274
Godfrey Lebo <emorylebo@gmail,com>
2021-08-14 16:13:27 +0100

    Second part of the header finished

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Image from "next/image";
-import { MenuIcon, SearchIcon } from "@heroicons/react/outline";
+import { ChatAltIcon, MenuIcon, SearchIcon } from "@heroicons/react/outline";
 
 function Header() {
   return (
-    <div className="flex items-center bg-gray-100 p-4 flex-grow py-2">
+    <div className="flex items-center bg-gray-100 p-2 flex-grow py-2">
       <div className="m-2 flex items-center flex-grow sm:flex-grow-0">
         <Image
           src="/grasperlogo.png"
@@ -15,7 +15,7 @@ function Header() {
         />
       </div>
       {/* Search */}
-      <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer  bg-yellow-400 hover:bg-yellow-500">
+      <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer  bg-yellow-400 hover:bg-yellow-300">
         <input
           className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
           type="text"
@@ -23,7 +23,20 @@ function Header() {
         <SearchIcon className="h-12 p-4" />
       </div>
       {/* Right */}
-      
+      <div className="text-black flex items-center  text-xs space-x-6 mx-6 whitespace-nowrap">
+        <div className="cursor-pointer link">
+          <p className="font-extrabold md:text-sm">Home</p>
+          <p className="font-extrabold md:text-sm">About</p>
+        </div>
+        <div className="cursor-pointer link">
+          <p className="font-extrabold md:text-sm">Team &</p>
+          <p className="font-extrabold md:text-sm">Testimonials</p>
+        </div>
+        <div className="relative flex items-center cursor-pointer link">
+          <ChatAltIcon className="h-6" />
+          <p className="hidden md:inline  font-extrabold">Contact</p>
+        </div>
+      </div>
     </div>
   );
 }
